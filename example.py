Correct amplitude normalization in fft_resample

example.py
@@ -22,8 +22,6 @@ output_len = int(Fs_out / Fs_in * input_len)
 L = int(0.05 * input_len / 2)
 print(output_len, L)
 
-def norm(x):
-    return x / np.max(np.abs(x))
 
 # %% Input signal
 
@@ -34,7 +32,7 @@ envelope = np.exp(-40 * (time % 1))
 
 input *= envelope
 
-input = norm(input)
+input = input / np.max(np.abs(input))
 
 if play:
     sd.play(input[:4*Fs_in], Fs_in)
@@ -42,7 +40,7 @@ if play:
 
 # %% FFT resampling without tapering
 
-output_naive = norm(fft_resample(input, np.ones(input_len), output_len))
+output_naive = fft_resample(input, np.ones(input_len), output_len)
 
 plt.plot(amp2db(output_naive[:5*Fs_out]))
 plt.show()
@@ -56,7 +54,7 @@ if play:
 
 taper, _ = get_taper(('ddc', 150), input_len, L)
 # taper, _ = get_taper(('cosine'), input_len, L)
-output_tapered = norm(fft_resample(input, taper, output_len))
+output_tapered = fft_resample(input, taper, output_len)
 
 plt.plot(amp2db(output_tapered[:5*Fs_out]))
 plt.show()
@@ -94,7 +92,7 @@ plt.fill_between(time_in[::decimate], low, hi, linewidth=1, ec='face')
 plt.xlim(0, input_len / Fs_in)
 plt.ylim(-1, 1)
 plt.axvspan(start-0.2, stop+0.2, 0.45, 0.55, color=(0, 0, 0, 0.12), linewidth=2, ec='red', ls='-')
-ax1.set_title('(a)', y=0, pad=-17)
+ax1.set_title('(a)', y=0, pad=-13)
 ax1.tick_params(pad=2)
 
 ax2 = plt.subplot(gs[1, 0])
@@ -102,20 +100,20 @@ plt.plot(time_in[mask_in], input[mask_in])
 plt.xlim(start, stop)
 plt.ylim(-0.01, 0.01)
 ax2.set_xlabel("Time (s)", labelpad=2)
-ax2.set_title('(b)', y=0, pad=-42)
+ax2.set_title('(b)', y=0, pad=-31)
 ax2.locator_params(axis='y', nbins=3)
 
 ax3 = plt.subplot(gs[1, 1], sharex=ax2, sharey=ax2)
 plt.plot(time_out[mask_out], output_naive[mask_out])
 plt.setp(ax3.get_yticklabels(), visible=False)
 ax3.set_xlabel("Time (s)", labelpad=2)
-ax3.set_title('(c)', y=0, pad=-42)
+ax3.set_title('(c)', y=0, pad=-31)
 
 ax4 = plt.subplot(gs[1, 2], sharex=ax2, sharey=ax2)
 plt.plot(time_out[mask_out], output_tapered[mask_out])
 plt.setp(ax4.get_yticklabels(), visible=False)
 ax4.set_xlabel("Time (s)", labelpad=2)
-ax4.set_title('(d)', y=0, pad=-42)
+ax4.set_title('(d)', y=0, pad=-31)
 
 
 plt.show()

lib/resamp.py
@@ -27,4 +27,4 @@ def fft_resample(input, taper, output_len):
 
         output_f *= taper
 
-    return np.sqrt(output_len / len(input)) * np.real(np.fft.ifft(output_f))
+    return ratio * np.real(np.fft.ifft(output_f))