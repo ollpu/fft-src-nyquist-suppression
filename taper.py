e714e0e4fc0bc1f08d5e2be928cf1e43788bc1ce
Roope Salmi <rpsalmi@gmail.com>
2025-09-04 12:10:22 +0300

    Better taper plot

--- taper.py
+++ taper.py
@@ -1,14 +1,12 @@
 # %%
+from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
 
 from lib.util import *
 from lib.taper import get_taper
 from lib.resamp import fft_resample
 
-plt.rcParams['font.family'] = 'Times New Roman'
-plt.rcParams["mathtext.fontset"] = 'cm'
-plt.rcParams['pdf.fonttype']=42
-plt.rcParams['lines.linewidth']=1
+plt.style.use('plots.mplstyle')
 
 M = 10000
 time_s = 1
@@ -18,24 +16,58 @@ L = 1000
 
 %matplotlib inline
 
-fig, axs = plt.subplots(1, 1, figsize=(5, 2.75), sharex=True, sharey=True)
-fig.subplots_adjust(left=0.04, bottom=0.24, right=0.98, top=0.97, wspace=0.1, hspace=0.45)
-freq = (np.arange(M) - M//2) / time_s
+fig = plt.figure(figsize=(3.5, 2))
+
+gs = GridSpec(2, 2, height_ratios=[2, 5], left=0.14, bottom=0.24, right=0.97, top=0.97, hspace=0.45, wspace=0.26)
+
+freq = (np.arange(M) - M//2) / time_s / 1000
+
+# axs[0].set_title("(a) Cosine", y=0, pad=-17)
+# axs[0].set_xmargin(0.02)
+# axs[0].set_ymargin(0.05)
+# axs[0].set_ylim(0, 1)
+
+# axs[1].set_xlabel("Frequency (Hz)", labelpad=1)
+# axs[1].set_title("(b) DDC optimal", y=0, pad=-42)
+
+lin = plt.subplot(gs[0, :])
+db_coarse = plt.subplot(gs[1, 0])
+db_fine = plt.subplot(gs[1, 1])
+
+lin.set_xmargin(0.02)
+lin.locator_params(min_n_ticks=6, axis='x')
+db_coarse.set_xlim(3.950, 5.050)
+db_coarse.set_ylim(-110, 10)
+db_fine.set_xlim(3.990, 4.210)
+db_fine.set_ylim(-1.1, 0.1)
+
+lin.axvspan(4.000, 5.000, color='#ddd')
+
+lin.set_ylabel('Amp.')
+db_coarse.set_ylabel('Magnitude (dB)')
+
+fig.supxlabel('Frequency (kHz)', x=0.56, y=0.06)
 
 taper, _ = get_taper('cosine', M, L)
 taper = np.fft.fftshift(taper)
-axs.plot(freq, amp2db(taper))
-axs.set_title("(a) Cosine", y=0, pad=-17)
-axs.set_xmargin(0.02)
-axs.set_ymargin(0.05)
-axs.set_ylim(-120, 0)
-axs.locator_params(min_n_ticks=4, steps=[1, 5, 10], axis='x')
+lin.plot(freq, taper, '-')
+db_coarse.plot(freq, amp2db(taper), '-', label='Cosine')
+db_fine.plot(freq, amp2db(taper), '-')
 
-taper, _ = get_taper(('ddc', 100), M, L)
+taper, _ = get_taper(('chebwin', 40), M, L)
 taper = np.fft.fftshift(taper)
-axs.plot(freq, amp2db(taper))
-axs.set_xlabel("Frequency (Hz)", labelpad=1)
-axs.set_title("(b) DDC optimal", y=0, pad=-42)
+lin.plot(freq, taper, '-')
+db_coarse.plot(freq, amp2db(taper), '-', label='Dolph–Cheb.')
+db_fine.plot(freq, amp2db(taper), '-')
+
+taper, _ = get_taper(('ddc', 90), M, L)
+taper = np.fft.fftshift(taper)
+col = '#000'
+lin.plot(freq, taper, ls=(0.5, (1, 1)), c=col)
+db_coarse.plot(freq, amp2db(taper), ls=(1, (1, 1.65)), c=col, label='DDC optimal')
+db_fine.plot(freq, amp2db(taper), ls=(1, (1, 1.65)), c=col)
+
+db_coarse.legend()
 
 plt.show()
 
