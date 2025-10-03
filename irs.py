caca3df0ff1e2b73ddd68246ac7100a974625572
Roope Salmi <rpsalmi@gmail.com>
2025-10-03 13:58:49 +0300

    Changes to IR figure

--- irs.py
+++ irs.py
@@ -10,16 +10,16 @@ plt.style.use('plots.mplstyle')
 N = 44100
 M = 192000
 time_s = 1
-L = 1000
+L = 1102
 
 tapers = {
     'Box': 'box',
     'Cosine': 'cosine',
-    'Hann': 'hann',
-    'Blackman': 'blackman',
-    'Dolph–Chebyshev': ('chebwin', 71),
-    'DDC $\\alpha=1/2$': ('ddc', 120, 0.5),
-    'DDC optimal': ('ddc', 120),
+    # 'Hann': 'hann',
+    # 'Blackman': 'blackman',
+    'Dolph–\nCheby.': ('chebwin', 63),
+    'DDC\t$\\alpha=1/2$': ('ddc', 120, 0.5),
+    'DDC\t$\\alpha$ optimal': ('ddc', 120),
 }
 
 
@@ -29,15 +29,22 @@ tapers = {
 input = np.zeros(N)
 input[N//2] = 1
 time = (np.arange(M) - M//2) / M * time_s * 1000
-mask = np.abs(time) < 22
+mask = np.abs(time) < 13
 
 box_taper, _ = get_taper('box', N, L)
 box_output = fft_resample(input, box_taper, M)
 box_output /= np.max(np.abs(box_output))
 
-fig = plt.figure(figsize=(3.5, 4))
-fig.subplots_adjust(left=0.14, bottom=0.13, right=0.98, top=0.995, wspace=0.1, hspace=0.3)
+fig = plt.figure(figsize=(3.5, 2.5))
+fig.subplots_adjust(left=0.14, bottom=0.20, right=0.98, top=0.995, wspace=0.1, hspace=0.3)
 axs = fig.subplots(len(tapers)//2, 2, sharex=True, sharey=True)
+
+fax = axs[0, 0]
+fax.set_xlim(-12.5, 12.5)
+fax.set_ylim(-165, 5)
+fax.locator_params(min_n_ticks=4, steps=[1, 2, 4, 10], axis='x')
+fax.locator_params(min_n_ticks=4, steps=[1, 2, 4, 10], axis='y')
+# fax.set_yticks([-180, -144, -120, -60, 0])
 
 for pos, (name, spec) in enumerate(tapers.items()):
     if spec == 'box': continue
@@ -52,24 +59,23 @@ for pos, (name, spec) in enumerate(tapers.items()):
     output /= np.max(np.abs(output))
 
     ax = axs[row][col]
-    ax.plot(time[mask], amp2db(box_output[mask]), label='Box', linewidth=0.7, color='#ccc')
+    ax.plot(time[mask], amp2db(box_output[mask]), label='Box', linewidth=0.7, color='#bbb')
     # ax.fill_between(time[mask], -300, amp2db(box_output[mask]), linewidth=1, color='#ccc', ec='#ccc')
     ax.plot(time[mask], amp2db(output[mask]), label=name, linewidth=0.7, c='tab:blue')
 
-    # ax.axhline(-100)
-    # ax.axhline(0)
+    ax.grid(True, lw=0.5, c='#ddd')
+    # ax.xaxis.set_zorder(1.5)
 
-    # ax.legend(loc='upper right')
-    ax.set_xlim(-16, 16)
-    ax.set_ylim(-165, 5)
-    ax.locator_params(min_n_ticks=4, steps=[1, 2, 5, 10], axis='x')
-    ax.locator_params(min_n_ticks=4, steps=[1, 2, 4, 10], axis='y')
-
-    pad = -13
+    pad = -11
     if last_row:
-        pad = -34
+        pad = -32
         ax.set_xlabel("Time (ms)", labelpad=3)
-    ax.set_title(f'({chr(ord('a')+pos-1)}) {name}', y=0, pad=pad)
+    ax.set_title(f'({chr(ord('a')+pos-1)})', y=0, pad=pad)
+    parts = name.split("\t")
+    ax.text(0.05, 0.93, parts[0], horizontalalignment='left', verticalalignment='top', transform=ax.transAxes)
+    if len(parts) > 1:
+        ax.text(0.95, 0.85, parts[1], horizontalalignment='right', verticalalignment='baseline', transform=ax.transAxes)
+
 
 fig.supylabel("Magnitude (dB)", x=0.01)
 
